5851c8fc465829abf3a6488106683f6b8029b7e5
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-08-02 15:25:46 +0200

    createpost method to create and return post

---
+++ controllers/postsController.js
@@ -0,0 +1,15 @@
+const Posts = require("../models/postModel");
+module.exports = {
+  createPost: async (req, res) => {
+    try {
+      const newPost = await Posts.create(req.body);
+      const post = await newPost.populate(
+        "author",
+        "fullName email dateOfBirth gender country profileImage bio socialMediaLinks createdAt"
+      );
+      return res.status(200).json({ message: "Your post is created", post });
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  },
+};
