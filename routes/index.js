4772782ec855f3594bb3f6b0d0bc17a01c51cb19
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-08-02 15:25:02 +0200

    create post routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userRoutes = require("./user");
+const postsRoutes = require("./posts");
 
 router.use("/user", userRoutes);
+router.use("/post", postsRoutes);
 
 module.exports = router;
