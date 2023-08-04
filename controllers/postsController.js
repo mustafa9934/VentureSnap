6157fbff26aee191e348620835c604d7d7a9d8d5
mustafa9934 <mustsfa.khan9434@gmail.com>
2023-08-04 23:27:28 +0200

    getting a single post with comments and populate comment author

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,5 @@
 const Posts = require("../models/postModel");
+const Comment = require("../models/commentModel");
 module.exports = {
   createPost: async (req, res) => {
     try {
@@ -19,6 +20,21 @@ module.exports = {
         "fullName email dateOfBirth gender country profileImage bio socialMediaLinks createdAt"
       );
       return res.status(200).json({ posts });
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  },
+  getSinglePost: async (req, res) => {
+    const postId = req.params.id;
+    try {
+      const post = await Posts.findById({ _id: postId });
+      const comments = await Comment.find({ postId }).populate(
+        "author",
+        "fullName email dateOfBirth gender country profileImage bio socialMediaLinks createdAt"
+      );
+      console.log(post);
+      console.log(comments);
+      res.send("getting a post");
     } catch (error) {
       return res.status(500).json({ message: error });
     }
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const verifyToken = require("../middlewares/postsMiddleware");
-const { createPost, getAllPosts } = require("../controllers/postsController");
+const {
+  createPost,
+  getAllPosts,
+  getSinglePost,
+} = require("../controllers/postsController");
 const router = express.Router();
 
 router.post("/create", verifyToken, createPost);
 router.get("/get-all", getAllPosts);
+router.get("/get/:id", getSinglePost);
 
 module.exports = router;
